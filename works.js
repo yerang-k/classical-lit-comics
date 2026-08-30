0e79af6f9fa56c5c8df0e8f09b6ab1e9237689bf
yerang-k <yeskyr0215@gmail.com>
2026-08-30 21:09:51 +0900

    구글 시트 API 주소 연결

--- works.js
+++ works.js
@@ -1,6 +1,6 @@
 // 작품 목록은 구글 시트에서 불러온다. 새 작품 등록은 시트에 "제목 + PDF 링크"를 한 줄 추가하면 된다.
 // 배포한 앱스스크립트 웹앱 주소(.../exec)를 여기에 붙여넣는다.
-const WORKS_API_URL = 'PASTE_APPS_SCRIPT_EXEC_URL_HERE';
+const WORKS_API_URL = 'https://script.google.com/macros/s/AKfycbySht7ttgztYAocAIG-_djogOAdWeVfktoebs0Y7eaAu-J04doU-3u4N4CY2mmo24euhg/exec';
 
 async function getWorks() {
   const res = await fetch(WORKS_API_URL);
