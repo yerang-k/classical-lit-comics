a7f8ead9104a5a6b8e51c3c535ff4f3685302f26
yerang-k <yeskyr0215@gmail.com>
2026-08-30 21:17:31 +0900

    PDF는 GitHub works 폴더에서, 시트는 제목+파일명만 관리하도록 변경

--- works.js
+++ works.js
@@ -1,5 +1,6 @@
-// 작품 목록은 구글 시트에서 불러온다. 새 작품 등록은 시트에 "제목 + PDF 링크"를 한 줄 추가하면 된다.
-// 배포한 앱스스크립트 웹앱 주소(.../exec)를 여기에 붙여넣는다.
+// 작품 목록은 구글 시트에서 불러온다. 새 작품 등록:
+// 1) PDF 파일을 GitHub 저장소의 works 폴더에 올린다
+// 2) 시트에 "제목 + 파일명" 한 줄을 추가한다
 const WORKS_API_URL = 'https://script.google.com/macros/s/AKfycbySht7ttgztYAocAIG-_djogOAdWeVfktoebs0Y7eaAu-J04doU-3u4N4CY2mmo24euhg/exec';
 
 async function getWorks() {
@@ -8,6 +9,6 @@ async function getWorks() {
   return res.json();
 }
 
-function pdfUrlFor(fileId) {
-  return 'https://drive.google.com/uc?export=download&id=' + encodeURIComponent(fileId);
+function pdfUrlFor(file) {
+  return 'works/' + file;
 }
