c22736452d886505e53bf70eddc0b3628fd6f83c
yerang-k <yeskyr0215@gmail.com>
2026-08-30 20:50:59 +0900

    고전문학 만화 뷰어 - 춘향전 추가

---
+++ works.js
@@ -0,0 +1,5 @@
+// 작품을 추가할 때는 이 배열에 한 줄만 더하면 됩니다.
+// slug: 주소에 쓰이는 영문 식별자, title: 화면에 보이는 제목, file: works/ 폴더 안 PDF 경로
+const WORKS = [
+  { slug: 'chunhyangjeon', title: '춘향전', file: 'works/chunhyangjeon.pdf' }
+];
